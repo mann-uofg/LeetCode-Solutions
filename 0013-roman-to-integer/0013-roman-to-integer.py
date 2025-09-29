a20a24514ec7a87c427ff4c68364de90f3f94411
Mann Modi <104613624+mann-uofg@users.noreply.github.com>
2025-09-30 02:36:19 +0530

    Time: 4 ms (63.74%), Space: 17.8 MB (64.53%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,17 +1,11 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         hashTable = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
-        i = 0
-        summ = 0
+        total = 0
 
-        while i < len(s):
-            if (i + 1) < len(s) and hashTable[s[i]] < hashTable[s[i + 1]]:
-                summ += hashTable[s[i + 1]] - hashTable[s[i]]
-                i += 2
+        for i in range(len(s)):
+            if i + 1 < len(s) and hashTable[s[i]] < hashTable[s[i + 1]]:
+                total -= hashTable[s[i]]
             else:
-                if i == len(s):
-                    summ += hashTable[s[i]]
-                else:
-                    summ += hashTable[s[i]]
-                    i += 1
-        return summ
+                total += hashTable[s[i]]
+        return total
