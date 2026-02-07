ddcd17cf078316fcd4db9d85cf566d0660dabe79
Mann Modi <104613624+mann-uofg@users.noreply.github.com>
2026-02-07 12:30:55 -0500

    Time: 13 ms (13.69%), Space: 25 MB (7.82%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,14 +2,17 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         hashmap = {}
         result = []
+        bucket = [[] for i in range(len(nums) + 1)]
 
         for num in nums:
             if num in hashmap:
                 hashmap[num] += 1
             else:
                 hashmap[num] = 1
-        for num in range(0, k):
-            max_element = max(hashmap, key = hashmap.get)
-            result.append(max_element)
-            hashmap.pop(max_element)
-        return result
+        for num in hashmap:
+            bucket[hashmap[num]].append(num)
+        for i in range(len(nums), 0, -1):
+            for num in bucket[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
