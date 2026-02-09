5364cb89c7d60a51713685358127b930452b80af
Mann Modi <104613624+mann-uofg@users.noreply.github.com>
2026-02-09 13:59:45 -0500

    Time: 0 ms (100%), Space: 21.3 MB (19.9%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,23 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        hashmap = {}
+        """
+        Brute force using hashmap/array method
+        """
+        # hashmap = {}
+
+        # for num in nums:
+        #     if num in hashmap:
+        #         hashmap[num] += 1
+        #     else:
+        #         hashmap[num] = 1
+        # for num in hashmap:
+        #     if hashmap[num] == 1:
+        #         return num
+        """
+        Bitwise method
+        """
+        result = 0
 
         for num in nums:
-            if num in hashmap:
-                hashmap[num] += 1
-            else:
-                hashmap[num] = 1
-        for num in hashmap:
-            if hashmap[num] == 1:
-                return num
+            result ^= num
+        return result
