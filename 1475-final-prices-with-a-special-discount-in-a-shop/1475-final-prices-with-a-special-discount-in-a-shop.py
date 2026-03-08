6b400e0c70476c2a9547329daaa0c470317d6d89
Mann Modi <104613624+mann-uofg@users.noreply.github.com>
2026-03-08 17:25:42 -0400

    Time: 3 ms (51.28%), Space: 19.3 MB (67.74%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,14 +1,32 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
+
+        # Brute-force Solution
+
+        # answer = prices[:]
+
+        # for i in range(len(prices)):
+        #     cursor = i + 1
+        #     while cursor != len(prices):
+        #         if prices[cursor] <= prices[i]:
+        #             answer[i] = prices[i] - prices[cursor]
+        #             break
+        #             cursor += 1
+        #         else:
+        #             cursor += 1
+        # return answer
+
+        # Monotonic Stack Approach
+
         answer = prices[:]
+        stack = []
 
         for i in range(len(prices)):
-            cursor = i + 1
-            while cursor != len(prices):
-                if prices[cursor] <= prices[i]:
-                    answer[i] = prices[i] - prices[cursor]
-                    break
-                    cursor += 1
+            while stack:
+                if prices[i] <= prices[stack[-1]]:
+                    answer[stack[-1]] = prices[stack[-1]] - prices[i]
+                    stack.pop()
                 else:
-                    cursor += 1
+                    break
+            stack.append(i)
         return answer
